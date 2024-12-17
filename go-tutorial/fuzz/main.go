de752cc7ba0c5d028d1eb5688f68d15ee11c34ea
hashiotoko <141216166+hashiotoko@users.noreply.github.com>
2024-12-17 10:51:27 +0900

    try to use fuzzing test

---
+++ go-tutorial/fuzz/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
+	fmt.Printf("original: %q\n", input)
+	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
+}
+
+// func Reverse(str string) string {
+// 	b := []byte(str)
+// 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+// 		b[i], b[j] = b[j], b[i]
+// 	}
+// 	return string(b)
+// }
+
+// Fix 1
+// func Reverse(str string) string {
+// 	fmt.Printf("input: %q\n", str) // debug
+// 	r := []rune(str)
+// 	fmt.Printf("rune: %q\n", r) // debug
+// 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+// 		r[i], r[j] = r[j], r[i]
+// 	}
+// 	return string(r)
+// }
+
+// FIx 2
+func Reverse(str string) (string, error) {
+	if !utf8.ValidString(str) {
+		return str, errors.New("input is not valid UTF-8")
+	}
+	r := []rune(str)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
+}
